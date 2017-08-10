d0f2de868165082bf35f503c42355409a9129ee6
Brian Smith <briaguya@gmail.com>
2017-08-10 14:15:43 -0500

    switching is working

--- src/components/Plugins/TextSwitchPlugin.js
+++ src/components/Plugins/TextSwitchPlugin.js
@@ -3,12 +3,13 @@ const textSwitchPlugin = {
   label: "Text Switch",  
   fromBlock: match => match && {
     defaultText: match[1],
-    alternateText: match[2]
+    alternateText: match[2],
+    randomId: Math.floor(Math.random() * 1000)
   },
   toBlock: data => `{{< textswitch {${ data.defaultText }} {${ data.alternateText }} >}}`,
   toPreview: data => `
-    <span id="default">${ data.defaultText }</span>
-    <span id="alternate" style="visibility: hidden">${ data.alternateText }</span>
+    <span id="default${ data.randomId }" onclick="document.getElementById('alternate${ data.randomId }').style.display='inline'; document.getElementById('default${ data.randomId }').style.display='none';">${ data.defaultText }</span>
+    <span id="alternate${ data.randomId }" style="display: none" onclick="document.getElementById('default${ data.randomId }').style.display='inline'; document.getElementById('alternate${ data.randomId }').style.display='none';">${ data.alternateText }</span>
   `,
   pattern: /^{{<\s?textswitch {(\S+)} {(\S+)}\s?>}}/,
   fields: [
